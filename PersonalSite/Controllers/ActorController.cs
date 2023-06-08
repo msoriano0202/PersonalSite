643163e7de8896718fdba8563a0963ebc8084d9a
Miguel Soriano <miguel_mst@hotmail.com>
2023-06-08 18:43:45 -0500

    Initial Commit

---
+++ PersonalSite/Controllers/ActorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly ILogger<ActorController> _logger;
+
+        public ActorController(ILogger<ActorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalSite/Controllers/SystemsEngineerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSite.Controllers
+{
+    public class SystemsEngineerController : Controller
+    {
+        private readonly ILogger<SystemsEngineerController> _logger;
+
+        public SystemsEngineerController(ILogger<SystemsEngineerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
